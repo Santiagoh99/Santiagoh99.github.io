e4a9da2e05d24f18a7590f587d9554e65cb6ef59
santiherrera99@icloud.com <santiherrera99@icloud.com>
2022-10-20 14:31:50 -0300

    images

--- src/components/Proyect.js
+++ src/components/Proyect.js
@@ -1,45 +1,56 @@
 import React, { useState } from 'react'
 import { Grid, Card, CardMedia, CardActionArea, CardContent, Typography, Divider, } from '@mui/material'
 
-
 const styles = {
     root: {
         maxWidth: 345,
     },
 }
 
+const images = [
+    {
+        url: '/static/images/buttons/breakfast.jpg',
+        title: 'Breakfast',
+    },
+    {
+        url: '/static/images/buttons/burgers.jpg',
+        title: 'Burgers',
+    },
+    {
+        url: '/static/images/buttons/camera.jpg',
+        title: 'Camera',
+    },
+];
+
 export default function Proyect() {
 
     return (
         <>
             <Grid container spacing={4}>
-                <Grid item xs={12} md={3}>
-                    <Card sx={styles.root} raised={true} elevation={3}>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                //image={src}
-                                title={'product.name'}
-                            />
-                            <CardContent>
-                                <Typography variant="h5">
-                                    product.id
-                                </Typography>
-                                <Typography variant="subtitle1" display="block" noWrap>
-                                    product.name
-                                </Typography>
-                                <Divider />
-                                <Typography variant="body2">
-                                    product.category
-                                </Typography>
-                                <Typography>
-                                    <strong>(product.price * (1 - (product.discount / 100))).toFixed(2)</strong>
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-
+                {images.map((image) => (
+                    <Grid item xs={12} md={4} key={image.url}>
+                        <Card sx={styles.root} raised={true}  key={image.url} elevation={3}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    image={image.url}
+                                />
+                                <CardContent>
+                                    <Typography variant="h5">
+                                        {image.title}
+                                    </Typography>
+                                    <Typography variant="subtitle1" display="block" noWrap>
+                                        subtitulo
+                                    </Typography>
+                                    <Divider />
+                                    <Typography variant="body2">
+                                        desc
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                ))}
             </Grid>
         </>
     )
