e0edffe97313ef02e3bad3315a33762ef02a1c2b
santiherrera99@icloud.com <santiherrera99@icloud.com>
2022-10-31 12:04:56 -0300

    contacto

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import { Grid,TextField } from '@mui/material'
+import { Button, Grid, TextField } from '@mui/material'
 
 export default function Contacto() {
 
     return (
-        <Grid container spacing={1} direction="column" justifyContent="center" alignItems="stretch">
-                <Grid item xs={12}>
-                    <TextField size='small' label='Nombre'></TextField>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField size='small' label='Email'></TextField>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField size='small' label='Telefono'></TextField>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField size='small' multiline maxRows={4} label='Mensaje'></TextField>
-                </Grid>
+        <Grid container spacing={2} direction="row" justifyContent="center" alignItems="flex-start">
+            <Grid item xs={12}>
+                <TextField size='small' label='Nombre'></TextField>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField size='small' label='Email'></TextField>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField size='small' label='Telefono'></TextField>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField size='small' multiline maxRows={4} label='Mensaje'></TextField>
+            </Grid>
+            <Grid item xs={12}>
+                <Button fullWidth>Enviar</Button>
+            </Grid>
         </Grid>
     )
 }
