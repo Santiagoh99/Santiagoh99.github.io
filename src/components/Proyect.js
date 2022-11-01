2d5a9d79ce80fe83fcd5fb9859aa684b95f03e0c
Santiagoh99 <santiherrera99@icloud.com>
2022-11-01 15:54:55 -0300

    proyect

--- src/components/Proyect.js
+++ src/components/Proyect.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Grid, Card, CardMedia, CardActionArea, CardContent, Typography, Divider, DialogContent, Dialog, Toolbar, DialogActions, Button, IconButton } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
+
 const styles = {
     root: {
         maxWidth: 345,
@@ -15,39 +16,33 @@ const styles = {
 
 const images = [
     {
-        url: '/static/images/buttons/breakfast.jpg',
+        url: require('../img/kosiuko.png'),
         title: 'KOSIUKO',
-        subtitle: 'E-commerce',
         desc: 'E-commerce de la marca Kosiuko para proveedores mayoristas, realizado con react/node , con backend en asp.net core y SQL server',
     },
     {
-        url: '/static/images/buttons/burgers.jpg',
+        url: require('../img/kosiuko.png'),
         title: 'KOSIUKO (TEP)',
-        subtitle: 'Produccion',
         desc: 'Desarrollo de Produccion de corte de la marca Kosiuko para integrar en Microsoft GP 2018, realizado con react/node , con backend en asp.net core y SQL server',
     },
     {
-        url: '/static/images/buttons/camera.jpg',
+        url: require('../img/kosiuko.png'),
         title: 'NONSTOP',
-        subtitle: 'Carga cliente',
         desc: 'Desarrollo de carga de clientes para la productora Nonstop, realizado con react/node , con backend en asp.net core y SQL server',
     },
     {
-        url: '/static/images/buttons/camera.jpg',
+        url: require('../img/kosiuko.png'),
         title: 'ALTIORA/IMPLEMENTACION INDUSTRIALES',
-        subtitle: 'Pagina Web',
         desc: 'Pagina principal de las consultoras Altiora y Implementaciones Industriales, realizado en HTML-CSS-Javascript',
     },
     {
-        url: '/static/images/buttons/camera.jpg',
+        url: require('../img/kosiuko.png'),
         title: 'FARMAGRAM',
-        subtitle: 'Produccion',
         desc: 'Desarrollo de Produccion de fallas de la empresa FarmaGram para integrar en Microsoft GP 2016, realizado con react/node , con backend en asp.net core y SQL server',
     },
     {
-        url: '/static/images/buttons/camera.jpg',
+        url: require('../img/kosiuko.png'),
         title: 'FARMAGRAM(Facturacion)',
-        subtitle: 'Produccion',
         desc: 'Desarrollo de produccion para la facturacion de la empresa FarmaGram para integrar en Microsoft GP 2016, realizado con react/node , con backend en asp.net core y SQL server',
     },
 ];
@@ -61,14 +56,11 @@ export default function Proyect() {
             {images.map((image) => (
                 <>
                     <Grid item xs='auto' key={image.url}>
-                        <Card style={styles.root} raised={true} key={image.url} elevation={3}>
+                    <Card style={styles.root} raised={true} key={image.url} elevation={3}>
                             <CardActionArea onClick={() => setOpen(true)}>
-                                <CardMedia component="img" image={image.url} />
+                                <CardMedia component="img" image={image.url} height="200"/>
                                 <CardContent>
                                     <Typography variant="h5" align='center'>{image.title}</Typography>
-                                    <Typography variant="subtitle1" display="block" noWrap align='center'>
-                                        {image.subtitle}
-                                    </Typography>
                                     <Divider />
                                     <Typography variant="body2" align='center'>{image.desc}</Typography>
                                 </CardContent>
@@ -77,15 +69,15 @@ export default function Proyect() {
                     </Grid>
                     <Dialog maxWidth='md' open={open} onClose={() => setOpen(false)}>
                         <Toolbar variant='dense' >
-                            <IconButton
-                                style={styles.closeButton}
-                                onClick={() => setOpen(false)}
-                                size='large'>
+                            <IconButton style={styles.closeButton} onClick={() => setOpen(false)} size='large'>
                                 <CloseIcon />
                             </IconButton>
                         </Toolbar>
                         <Divider />
                         <DialogContent>
+                            <video width="750" height="500" controls >
+                                <source src={image.video} type="video/mp4" />
+                            </video>
                         </DialogContent>
                         <DialogActions>
                             <Button onClick={() => setOpen(false)} variant='contained' color='primary' disableElevation fullWidth>
