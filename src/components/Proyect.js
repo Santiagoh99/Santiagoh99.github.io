04544ba7b489306b0cd4b96c949350376322a005
Santiagoh99 <santiherrera99@icloud.com>
2022-11-02 16:58:59 -0300

    margin

--- src/components/Proyect.js
+++ src/components/Proyect.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Grid, Typography, DialogContent, Dialog, DialogActions, Button, DialogTitle } from '@mui/material'
+import { Grid, Typography, DialogContent, Dialog, DialogActions, Button, DialogTitle, Divider } from '@mui/material'
 import { ImageButton, ImageSrc, Image, ImageBackdrop, ImageMarked } from "../components/imageButton"
 import { images } from '../data/image'
 
@@ -10,43 +10,55 @@ const styles = {
         pt: 2,
         pb: (theme) => `calc(${theme.spacing(1)} + 6px)`
     },
+    titlestyles: {
+        backgroundColor: '#393939',
+         color: 'white',
+          textAlign: 'center',
+           fontSize: 16
+    },
 }
 
 export default function Proyect() {
 
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false)
+    const [desc, setDesc] = useState(null)
+    const [film, setFilm] = useState(null)
+
+    function video(item) {
+        setDesc(item.desc)
+        setFilm(item.film)
+        setOpen(true)
+    }
 
     return (
         <Grid container justifyContent="center" alignItems="center">
             {images.map((image) => (
-                <>
-                    <Grid item xs={4} key={image.title} align="center">
-                        <ImageButton focusRipple key={image.tile} style={{ width: '50%' }} onClick={() => setOpen(true)}>
-                            <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
-                            <ImageBackdrop className="MuiImageBackdrop-root" />
-                            <Image>
-                                <Typography component="span" variant="subtitle1" color="inherit" sx={styles.imagenstyles}>
-                                    {image.title}
-                                    <ImageMarked className="MuiImageMarked-root" />
-                                </Typography>
-                            </Image>
-                        </ImageButton>
-                    </Grid>
-                    <Dialog maxWidth='md' open={open} onClose={() => setOpen(false)}>
-                        <DialogTitle style={{ backgroundColor: '#393939', color: 'white', textAlign: 'center', fontSize: 16 }}>{image.desc}</DialogTitle>
-                        <DialogContent>
-                            <video width="850" height="400" controls >
-                                <source src={image.video} type="video/mp4" />
-                            </video>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={() => setOpen(false)} variant='contained' color='primary' fullWidth>
-                                Cerrar
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
-                </>
+                <Grid item xs={4} key={image.title} align="center" sx={{margin:'10px'}}>
+                    <ImageButton focusRipple style={{ width: '80%' }} onClick={() => video(image)}>
+                        <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+                        <ImageBackdrop className="MuiImageBackdrop-root" />
+                        <Image>
+                            <Typography component="span" variant="subtitle1" color="inherit" sx={styles.imagenstyles}>
+                                {image.title}
+                                <ImageMarked className="MuiImageMarked-root" />
+                            </Typography>
+                        </Image>
+                    </ImageButton>
+                </Grid>
             ))}
+            <Dialog maxWidth='md' open={open} onClose={() => setOpen(false)}>
+                <DialogTitle style={styles.titlestyles}>{desc}</DialogTitle>
+                <DialogContent>
+                    <video width="850" height="400" controls >
+                        <source src={film} type="video/mp4" />
+                    </video>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)} variant='contained' color='primary' fullWidth>
+                        Cerrar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Grid>
     )
 }
