739d38b20fb044996684d37f22eba93dbc1a5d28
Santiagoh99 <santiherrera99@icloud.com>
2023-09-15 18:39:59 -0300

    url

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={esLocale}>
           <CssBaseline />
-          <Router basename="/Portafolio">
+          <Router basename="homepage">
             <Container>
               <NavBar />
             </Container>
